c805edd86bb1dcb2d6272bb6fe8cf280cbcb9c2d
roopahn184 <rooparoopahn184@gmail.com>
2023-01-06 11:02:46 +0530

    ok

---
+++ js-1/datatypes/product.js
@@ -0,0 +1,12 @@
+var price = 299
+var name = "check shirt"
+var rate = 3.9;
+var discount = true;
+var avail;
+var check = 'y'
+console.log(typeof price)
+console.log(typeof name)
+console.log(typeof rate)
+console.log(typeof discount)
+console.log(typeof avail)
+console.log(typeof check)
