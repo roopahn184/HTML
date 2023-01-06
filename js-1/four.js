64b90ec5dcc12494f1613c96132b389db0294483
roopahn184 <rooparoopahn184@gmail.com>
2023-01-06 13:03:33 +0530

    okey

---
+++ js-1/four.js
@@ -0,0 +1,18 @@
+var a = 10
+var b = 20
+var c = 35.5
+var d = "40"
+var e = "rahul"
+var f = "gandhi"
+var g = true
+var h = false
+var i;
+console.log(a*b)
+console.log(a*c)
+console.log(a*d)
+console.log(a*e)
+console.log(a*f)
+console.log(a*g)
+console.log(a*h)
+console.log(a*i)
+console.log(e*f)
---
+++ js-1/ternary.js
@@ -0,0 +1,8 @@
+var sal = 45000
+// if(sal > 40000){
+//     console.log("eligible")
+// }
+// else{
+//     console.log("not eligible")
+// }
+sal > 40000 ? console.log("eligible"):console.log("not eligible")
