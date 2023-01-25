77425bb04ee451cae003c2b404bb3a9a6e9aacd6
roopahn184 <rooparoopahn184@gmail.com>
2023-01-25 16:03:52 +0530

    ok

---
+++ js-1/assignments/arraymethods/one.js
@@ -0,0 +1,5 @@
+let arr = [10, 20, 30, 40]
+arr.push(50)
+console.log(arr)
+let removed_value = arr.pop()
+console.log(removed_value)
---
+++ js-1/assignments/arraymethods/two.js
@@ -0,0 +1,8 @@
+let arr = [10, 20, 30, 40]
+arr.push(50)
+arr.unshift(5)
+console.log(arr)
+
+arr.pop(3)
+arr.shift(0)
+console.log(arr)
