4c59111e5281b138bd9bc21a922d96bdec14ad91
roopahn184 <rooparoopahn184@gmail.com>
2023-03-08 21:27:38 +0530

    ok

---
+++ practice js/index.js
@@ -0,0 +1,32 @@
+// document.getElementById("count").innerText= 5
+// let firstbatch = 5
+// let secondbatch = 7
+// let count = firstbatch + secondbatch
+// console.log(count);
+// let myAge = 22
+// console.log(myAge);
+// let myAge = 22;
+// let humanDogRatio = 7;
+// let myDogAge = myAge * humanDogRatio
+// console.log(myDogAge);
+
+// let bonusPoints = 50;
+// console.log(bonusPoints);
+// bonusPoints = bonusPoints + 50
+// console.log(bonusPoints);
+// bonusPoints = bonusPoints - 75
+// console.log(bonusPoints);
+// bonusPoints = bonusPoints + 45
+// console.log(bonusPoints);
+// 
+let countEl = document.getElementById("count-el")
+let count = 0
+
+function increment() {
+   count = count + 1
+   countEl.innerText = count
+}
+
+function save() {
+    
+}
---
+++ practice js/index1.js
@@ -0,0 +1,4 @@
+let name = "roopa";
+let greeting = "hi, my name is";
+let mygreeting =  greeting + name;
+console.log(mygreeting);
