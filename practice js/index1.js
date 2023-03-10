1a949b84c46fb191a6610f2ee2a21de8704ce70c
roopahn184 <rooparoopahn184@gmail.com>
2023-03-10 09:33:07 +0530

    okie

--- practice js/index1.js
+++ practice js/index1.js
@@ -1,4 +1,8 @@
-let name = "roopa";
-let greeting = "hi, my name is";
-let mygreeting =  greeting + name;
-console.log(mygreeting);
+// let name = "roopa";
+// let greeting = "hi, my name is";
+// let mygreeting =  greeting + name;
+// console.log(mygreeting);
+let WelcomeEl = document.getElementById("Welcome-el");
+let name = "roopa"
+let greeting = "Welcome back"
+WelcomeEl.innerText = greeting + name
